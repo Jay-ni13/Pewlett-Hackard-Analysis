722f77b870246f267c2fe9e24d63c661b469b441
Jay-ni13 <jennifer.cockerill13@gmail.com>
2022-11-02 00:21:37 -0500

    Updating Queries and Images files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -45,3 +45,65 @@ FROM employees AS e
 ORDER BY e.emp_no;
 
 SELECT * FROM mentorship_eligibility;
+
+-- Supplemental
+-- Retrieve retiring count by department.
+SELECT COUNT(ut.emp_no) AS "count_r", ed.dept_no, d.dept_name
+INTO retire_count_dept
+FROM unique_titles AS ut
+	INNER JOIN employee_dept AS ed
+		ON ut.emp_no = ed.emp_no
+	INNER JOIN departments AS d
+		ON ed.dept_no = d.dept_no
+	WHERE ed.to_date = '9999-01-01'
+GROUP BY ed.dept_no, d.dept_name
+ORDER BY count_r DESC;
+
+SELECT * FROM retire_count_dept;
+
+-- Retirement counts by year
+SELECT COUNT (e.emp_no) AS "count_year_1"
+FROM employees AS e
+INNER JOIN employee_dept AS ed
+ON e.emp_no = ed.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1952-12-31')
+AND ed.to_date = '9999-01-01'
+ORDER BY count_year_1;
+
+SELECT COUNT (e.emp_no) AS "count_year_2"
+FROM employees AS e
+INNER JOIN employee_dept AS ed
+ON e.emp_no = ed.emp_no
+WHERE (e.birth_date BETWEEN '1953-01-01' AND '1953-12-31')
+AND ed.to_date = '9999-01-01'
+ORDER BY count_year_2;
+
+SELECT COUNT (e.emp_no) AS "count_year_3"
+FROM employees AS e
+INNER JOIN employee_dept AS ed
+ON e.emp_no = ed.emp_no
+WHERE (e.birth_date BETWEEN '1954-01-01' AND '1954-12-31')
+AND ed.to_date = '9999-01-01'
+ORDER BY count_year_3;
+
+SELECT COUNT (e.emp_no) AS "count_year_4"
+FROM employees AS e
+INNER JOIN employee_dept AS ed
+ON e.emp_no = ed.emp_no
+WHERE (e.birth_date BETWEEN '1955-01-01' AND '1955-12-31')
+AND ed.to_date = '9999-01-01'
+ORDER BY count_year_4;
+
+-- Eligible mentors by dept
+SELECT COUNT (me.emp_no) AS "count_m", ed.dept_no, d.dept_name
+INTO mentorship_by_dept
+FROM mentorship_eligibility AS me
+	INNER JOIN employee_dept AS ed
+		ON me.emp_no = ed.emp_no
+	INNER JOIN departments AS d
+		ON ed.dept_no = d.dept_no
+	WHERE (ed.to_date = '9999-01-01')
+GROUP BY ed.dept_no, d.dept_name
+ORDER BY count_m DESC;
+
+SELECT * FROM mentorship_by_dept;
